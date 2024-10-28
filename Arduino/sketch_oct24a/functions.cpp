6bbc01bd465d4f35da37efe80262f68bc051dd1f
Siroj K <167022994+kafleSiroj@users.noreply.github.com>
2024-10-28 11:43:31 +0545

    functions.cpp

--- Arduino/sketch_oct24a/functions.cpp
+++ Arduino/sketch_oct24a/functions.cpp
@@ -1,6 +1,8 @@
 #include "functions.h"
 #include <Arduino.h>
 
+Servo myServo;
+
 void stop() {
   analogWrite(ena1, 0);
   analogWrite(ena2, 0);
@@ -24,6 +26,7 @@ void reverse() {
   digitalWrite(m2p1, LOW);
   digitalWrite(m2p2, HIGH);
   analogWrite(ena2, 255);
+  delay(15 * 60); 
 }
 
 int dist() {
@@ -34,4 +37,30 @@ int dist() {
   duration = pulseIn(echo, HIGH);
 
   return duration * 0.034 / 2;
+}
+
+void turnLeft() {
+  analogWrite(ena1, 0);
+
+  digitalWrite(m2p1, HIGH);
+  digitalWrite(m2p2, LOW);
+  analogWrite(ena2, 255);
+
+  delay(500);
+  stop();
+}
+
+void turnRight() {
+  digitalWrite(m1p1, HIGH);
+  digitalWrite(m1p2, LOW);
+  analogWrite(ena1, 255);
+
+  analogWrite(ena2, 0);
+
+  delay(500);
+  stop();
+}
+
+void servoLeft() {
+  myServo.write(180);
 }
