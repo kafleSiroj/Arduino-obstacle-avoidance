1154dcf25bc565967a948ced759dd03d3fb53a14
Siroj K <167022994+kafleSiroj@users.noreply.github.com>
2024-10-28 22:03:48 +0545

    sketch_oct24a.ino

--- Arduino/sketch_oct24a/sketch_oct24a.ino
+++ Arduino/sketch_oct24a/sketch_oct24a.ino
@@ -9,11 +9,14 @@ int m2p2 = 6;
 int ena2 = 4;
 int echo = 12;
 int trig = 13;
+int cm = 10;
 long duration;
+int stopedDist = 0;
 
 
 
 void setup() {
+  Serial.begin(9600);
   pinMode(3, OUTPUT);
   pinMode(2, OUTPUT);
   pinMode(2, OUTPUT);
@@ -28,10 +31,27 @@ void setup() {
 
 void loop() {
   int distance = dist();
-  forward();
 
-  if (distance<20) {
+  if (distance < 10) {
+
+    stopedDist = distance;
     reverse();
+
   }
+
+  else if (distance>=(stopedDist+10) && distance<=(stopedDist+15)) {
+    stop();
+    checkDist();  
+    delay(400);
+  } 
+  
+  else {
+    myServo.write(90);
+    forward();
+  
+  }
+
+  Serial.println(distance);
   delay(100);
 }
+
