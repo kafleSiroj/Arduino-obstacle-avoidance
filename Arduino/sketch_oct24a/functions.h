eddfa90130a12b7c06b1fb42bcf48f2ca2c738c5
Siroj K <167022994+kafleSiroj@users.noreply.github.com>
2024-10-28 11:43:12 +0545

    functions.h

--- Arduino/sketch_oct24a/functions.h
+++ Arduino/sketch_oct24a/functions.h
@@ -1,5 +1,6 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
+#include <Servo.h>
 
 extern int m1p1;
 extern int m1p2;
@@ -11,10 +12,14 @@ extern int echo;
 extern int trig;
 extern long duration;
 
+extern Servo myServo;
 
 int dist(); 
 void stop();
 void forward();
 void reverse();
-
+void turnRight();
+void turnLeft();
+void servoRight();
+void servoLeft();
 #endif
